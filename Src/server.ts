3dc3e90720c7bea9fd98042daa54c5a1d7999b54
Ste-Janu <122880008+Ste-Janu@users.noreply.github.com>
2023-03-10 15:54:20 -0300

    .

--- Src/server.ts
+++ Src/server.ts
@@ -13,7 +13,7 @@ AppDataSource.initialize().then(() => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     next();
-  });
+  }); 
   
   app.post('/aeroporto', new AeroportoController().create);
   app.get('/aeroporto', new AeroportoController().getAll);
